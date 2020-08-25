f06810a79b24d0eac5c924474ba02191540e9d67
Yeriel Kim <asap0208@gmail.com>
2020-08-25 14:44:53 +0900

    프로젝트 마무리

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 import os
 
 SECRET_KEY = os.environ["MY_SECRET_KEY"]
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
